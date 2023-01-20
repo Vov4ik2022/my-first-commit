90e9f39693b3e67f9211e3cba036fa0aaaeb0675
Terentyev Vladimir <Terentyev_Vladimir@ukr.net>
2023-01-20 21:52:15 +0200

    test1

---
+++ javascript/scripr.js
@@ -0,0 +1,34 @@
+function splitMessage(message, delimiter) {
+  let words;
+  // Change code below this line
+   return message.split(" ");
+
+
+  // Change code above this line
+  return words;
+}
+
+splitMessage("Mango hurries to the train", " ");
+splitMessage("Mango", "");
+splitMessage("best_for_week", "_");
+
+
+// Дополни код функции splitMessage(message, delimiter) так,
+//     чтобы она возвращала в переменной words результат разделения
+// строки message по разделителю delimiter - массив строк
+
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+
+//   let firstElementsIndex = 0;
+//   let firstElements = array[firstElementsIndex];
+//   let lastElementsIndex = array.length - 1;
+//   let lastElements = array[lastElementsIndex];
+//   return [firstElements, lastElements];
+//   // Change code above this line
+// }
+
+// getExtremeElements([1, 2, 3, 4, 5]);
+// getExtremeElements(["Earth", "Mars", "Venus"]);
+// getExtremeElements(["apple", "peach", "pear", "banana"]);
